bec24939c25044e4bd9fcbc7eb0ea1443de449a3
jk <jan.kramar@posteo.de>
2017-09-11 15:55:35 +0200

    Instelling brew, changing the shell and creating a Sites folder

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-#Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Starting...."
+
+# Homebrew
+if test ! $(which brew); then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+brew tap homebrew/bundle
+brew bundle
+
+# ZSH
+chsh -s $(which zsh)
+
+# Sites directory
+mkdir $HOME/Sites
+
